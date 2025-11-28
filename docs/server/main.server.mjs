7a16be4a35ac607b591490d6c06540ec362d7f6c
Giorgos Ndinas <giorgosndinas@gmail.com>
2025-11-28 18:06:56 +0200

    Build Test

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-VG5TRLIO.mjs";import"./chunk-L44CGASG.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
--- docs/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{jc as o,kc as r,lc as n,wb as e}from"./chunk-L44CGASG.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
