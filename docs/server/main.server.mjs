89abb85ec51ec811542882647c8badd51388d4bd
Giorgos Ndinas <giorgosndinas@gmail.com>
2025-11-28 17:56:45 +0200

    Test build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WDRYYDDP.mjs";import"./chunk-H23TKBBD.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-VG5TRLIO.mjs";import"./chunk-L44CGASG.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
